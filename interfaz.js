c701d119a3c69a913f85b66135c991c38de3d85d
fabi8301 <fabianregunega83@gmail.com>
2024-02-17 10:03:54 -0300

    cambios totales

---
+++ interfaz.js
@@ -0,0 +1,31 @@
+/*
+// creando nodos de nuestra pagina
+//Campos de texto
+const txtnombres = document.getElementById("txtNombres");
+const txtapellidos  = document.getElementById("txtApellidos");
+
+//Array de objetos del radiobutton
+//Presta atención, usamos el getElementsByName
+const arrayRadioSexo = document.getElementsByName("sexo");
+
+//Botones
+const btnCrear = document.getElementById("btnCrear");
+const btnSalir = document.getElementById("btnSalir");
+
+//Se le asigna el evento click al botón crear
+btnCrear.addEventListener("click"), function() {
+
+    //esto recupera el valor de nombre
+    const nombres = txtnombres.value.trim();
+    //valido el nombre
+    if(!nombres) {
+        alert("Falta el nombre");
+        return;
+    }*/
+   // ------------------------------------
+   /* function musica(audio,loop){
+        let music = new audio(audio)
+        music.loop = loop;
+        music.play();
+}
+musica("media/musicaminecraft.mp3",true)*/
